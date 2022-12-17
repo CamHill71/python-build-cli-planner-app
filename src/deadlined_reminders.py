1f4843f536c4de765e61dec27bd982e1135106c2
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-17 23:28:41 +0800

    s

---
+++ src/deadlined_reminders.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 17 22:27:50 2022
+
+@author: Cameron Hill
+"""
+
+from abc import ABCMeta,abstractmethod,ABC
+from collections.abc import Iterable
+from dateutil.parser import parse
+from datetime import datetime
+
+
+class DeadlinedMetaReminder(ABCMeta):
+
+    @abstractmethod
+    def is_due():
+        pass
+
+
+class DeadlinedReminder(ABC,Iterable):
+
+    @abstractmethod
+    def is_due():
+        pass
+
+    def __iter__(self,text,formatted_date):       
+        return iter([text, formatted_date])
+
+
+class Datereminder(DeadlinedReminder):
+    def __init__(self,text,date) -> None:        
+        self.date = parse(date,dayfirst=True)
+        self.text = text
